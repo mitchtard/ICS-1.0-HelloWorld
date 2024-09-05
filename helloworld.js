46095810fa43b2f3685616cab762df1301c71bb8
him <1watkinsmit@hdsb.ca>
2024-09-05 16:34:16 +0000

    Added 5 Logs

--- helloworld.js
+++ helloworld.js
@@ -1,3 +1,8 @@
 // Log "Hello World" to the console.
 // Log four additional messages of your choosing. 
 // Comment each message appropriately. 
+console.log("Hello World")
+console.log("My name is Mitchel")
+console.log("I guess")
+console.log("woof woof bark bark")
+console.log("And thats how I met your mother")
